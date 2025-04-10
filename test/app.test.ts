5a8b5bfe9199793bf45b31d301e72d3be76d93fa
thermodynathan <sh4aj@virginia.edu>
2025-04-09 17:03:04 -0700

    add basic integration tests for receipt processing

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,25 +1,21 @@
-import request from 'supertest';
+import request from "supertest";
 
-import app from '../src/app';
+import app from "../src/app";
 
-describe('app', () => {
-  it('responds with a not found message', (done) => {
-    request(app)
-      .get('/non-existent-route')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(404, done);
+describe("app", () => {
+  it("responds with a not found message when given a non-existent route", (done) => {
+    request(app).get("/non-existent-route").expect(404, done);
   });
 });
 
-describe('GET /', () => {
-  it('responds with a json message', (done) => {
-    request(app)
-      .get('/')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, {
-        message: 'Receipts Processing Service is running',
-      }, done);
+describe("GET /", () => {
+  it("responds with a json message", (done) => {
+    request(app).get("/").set("Accept", "application/json").expect("Content-Type", /json/).expect(
+      200,
+      {
+        message: "Receipts Processing Service is running",
+      },
+      done,
+    );
   });
 });
---
+++ test/receipts.test.ts
@@ -0,0 +1,50 @@
+import request from "supertest";
+import app from "../src/app";
+
+const VALID_RECEIPT = {
+  retailer: "Target",
+  purchaseDate: "2022-01-01",
+  purchaseTime: "13:01",
+  items: [
+    {
+      shortDescription: "Mountain Dew 12PK",
+      price: "6.49",
+    },
+    {
+      shortDescription: "Emils Cheese Pizza",
+      price: "12.25",
+    },
+    {
+      shortDescription: "Knorr Creamy Chicken",
+      price: "1.26",
+    },
+    {
+      shortDescription: "Doritos Nacho Cheese",
+      price: "3.35",
+    },
+    {
+      shortDescription: "   Klarbrunn 12-PK 12 FL OZ  ",
+      price: "12.00",
+    },
+  ],
+  total: "35.35",
+};
+
+describe("Sending a valid receipt to the API results in the receipt being processed and score persisted", () => {
+  let receiptId: string;
+  it("returns a 200 status code and a receipt id when given a valid receipt", async () => {
+    const response = await request(app).post("/receipts/process").send(VALID_RECEIPT).expect(200);
+    expect(response.body.id).toBeDefined();
+    expect(typeof response.body.id).toBe("string");
+    receiptId = response.body.id;
+  });
+  it("allows the user to retrieve the score for a receipt", async () => {
+    const response = await request(app).get(`/receipts/${receiptId}/points`).expect(200);
+    expect(response.body.points).toBeDefined();
+    expect(typeof response.body.points).toBe("number");
+  });
+  it("returns a 404 status code when the receipt id is not found", async () => {
+    const response = await request(app).get(`/receipts/1234567890/points`).expect(404);
+    expect(response.body.message).toBe("Receipt not found");
+  });
+});
